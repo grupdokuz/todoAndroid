d3a143504067185f92275271760b461c718af7d9
ugurtufekci <ugurtufekci.tobbetu@gmail.com>
2017-07-19 23:43:45 +0300

    Text field and 'done' button created

--- app/src/main/java/com/example/ugur/todolistapp/MainActivity.java
+++ app/src/main/java/com/example/ugur/todolistapp/MainActivity.java
@@ -2,12 +2,21 @@ package com.example.ugur.todolistapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-
+    ListView listView;
+    ArrayList<String> arrayList;
+    ArrayAdapter<String>arrayAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 }
+
+
+
