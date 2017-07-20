f479c9decb222142cdf06a1b62811d4d6ca07fe2
ugurtufekci <ugurtufekci.tobbetu@gmail.com>
2017-07-20 18:21:03 +0300

    DbHelper

--- app/src/main/java/com/example/ugur/todolistapp/MainActivity.java
+++ app/src/main/java/com/example/ugur/todolistapp/MainActivity.java
@@ -8,14 +8,31 @@ import android.widget.ListView;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
     ListView listView;
+    DbHelper dbHelper;
     ArrayList<String> arrayList;
     ArrayAdapter<String>arrayAdapter;
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+       // setContentView(R.layout.big_layout);
+
+        dbHelper = new DbHelper(this);
+        listView = (ListView)findViewById(R.id.listView);
+      //  loadTaskList();
     }
+    //////////////////////////////////////
+    private void loadTaskList(){
+        ArrayList<String> taskList = dbHelper.getTaskList();
+
+
+        //Adapter add and clear
+    }
+    //////////////////////////////////////
+
 }
 
 
