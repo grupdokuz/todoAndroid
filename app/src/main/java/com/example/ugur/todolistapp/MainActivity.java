27360e31ec9d64dc9773cd05030a3eaacaa2920c
ugurtufekci <ugurtufekci.tobbetu@gmail.com>
2017-07-29 22:15:48 +0300

    Intent Codes and textfield control added.There is a small bug appeared, I think this bug caused by the layouts.

--- app/src/main/java/com/example/ugur/todolistapp/MainActivity.java
+++ app/src/main/java/com/example/ugur/todolistapp/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.ugur.todolistapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -10,7 +12,8 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
 
     ListView listView;
-    DbHelper dbHelper;
+    //DbHelper dbHelper;
+    String taskText;
     ArrayList<String> arrayList;
     ArrayAdapter<String>arrayAdapter;
     @Override
@@ -20,17 +23,37 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
        // setContentView(R.layout.big_layout);
 
-        dbHelper = new DbHelper(this);
+        //dbHelper = new DbHelper(this);
         listView = (ListView)findViewById(R.id.listView);
+        arrayList = new ArrayList<>();
+        arrayAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,arrayList);
+        listView.setAdapter(arrayAdapter);
       //  loadTaskList();
     }
+
+    public void onClick(View v){
+        Intent intent = new Intent();
+        intent.setClass(MainActivity.this,EditClass.class);
+        startActivityForResult(intent, IntentClass.INTENT_CODE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode==IntentClass.INTENT_RESULT_CODE){
+            taskText=data.getStringExtra(IntentClass.INTENT_TASK_FIELD);
+            arrayList.add(taskText);
+            arrayAdapter.notifyDataSetChanged();
+        }
+    }
+
     //////////////////////////////////////
-    private void loadTaskList(){
+    /*private void loadTaskList(){
         ArrayList<String> taskList = dbHelper.getTaskList();
 
 
         //Adapter add and clear
-    }
+    }*/
     //////////////////////////////////////
 
 }
