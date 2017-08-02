106a7c3a852c2976ebd4f3151292e6f7c610795c
mkutlu <mkutlu@etu.edu.tr>
2017-08-02 13:50:40 +0300

    HTTP Handler added, now all server todos can list on android.2

--- app/src/main/java/com/example/ugur/todolistapp/MainActivity.java
+++ app/src/main/java/com/example/ugur/todolistapp/MainActivity.java
@@ -1,62 +1,136 @@
 package com.example.ugur.todolistapp;
-
-import android.content.Intent;
+import android.util.Log;
+import android.widget.Toast;
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import org.json.*;
+import java.util.ArrayList;
+import java.util.List;
+import android.os.AsyncTask;
+import java.io.*;
+import android.app.Activity;
+import android.app.ProgressDialog;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
 
-    ListView listView;
-    //DbHelper dbHelper;
-    String taskText;
-    ArrayList<String> arrayList;
-    ArrayAdapter<String>arrayAdapter;
-    @Override
+    private String TAG = MainActivity.class.getSimpleName();
+    private ListView lv;
 
+    ArrayList<HashMap<String, String>> contactList;
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-       // setContentView(R.layout.big_layout);
 
-        //dbHelper = new DbHelper(this);
-        listView = (ListView)findViewById(R.id.listView);
-        arrayList = new ArrayList<>();
-        arrayAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,arrayList);
-        listView.setAdapter(arrayAdapter);
-      //  loadTaskList();
+        contactList = new ArrayList<>();
+        lv = (ListView) findViewById(R.id.list);
+
+        new GetContacts().execute();
     }
 
-    public void onClick(View v){
-        Intent intent = new Intent();
-        intent.setClass(MainActivity.this,EditClass.class);
-        startActivityForResult(intent, IntentClass.INTENT_CODE);
-    }
+    private class GetContacts extends AsyncTask<Void, Void, Void> {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            Toast.makeText(MainActivity.this,"Json Data is downloading",Toast.LENGTH_LONG).show();
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if(resultCode==IntentClass.INTENT_RESULT_CODE){
-            taskText=data.getStringExtra(IntentClass.INTENT_TASK_FIELD);
-            arrayList.add(taskText);
-            arrayAdapter.notifyDataSetChanged();
+        }
+
+        @Override
+        protected Void doInBackground(Void... arg0) {
+            HttpHandler sh = new HttpHandler();
+            // Making a request to url and getting response
+            String url = "http://192.168.1.31:3001/api/todos";
+            String jsonStr = sh.makeServiceCall(url);
+
+            Log.e(TAG, "Response from url: " + jsonStr);
+            if (jsonStr != null) {
+                try {
+                    JSONArray deneme = new JSONArray(jsonStr);
+
+                    // Getting JSON Array node
+                    JSONArray contacts = deneme;
+
+                    // looping through All Contacts
+                    for (int i = 0; i < contacts.length(); i++) {
+                        JSONObject c = contacts.getJSONObject(i);
+                        String id = c.getString("id");
+                        String title = c.getString("title");
+
+                        // Phone node is JSON Object
+
+
+                        // tmp hash map for single contact
+                        HashMap<String, String> contact = new HashMap<>();
+
+                        // adding each child node to HashMap key => value
+                        contact.put("id", id);
+                        contact.put("title", title);
+
+                        // adding contact to contact list
+                        contactList.add(contact);
+                    }
+                } catch (final JSONException e) {
+                    Log.e(TAG, "Json parsing error: " + e.getMessage());
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(getApplicationContext(),
+                                    "Json parsing error: " + e.getMessage(),
+                                    Toast.LENGTH_LONG).show();
+                        }
+                    });
+
+                }
+
+            } else {
+                Log.e(TAG, "Couldn't get json from server.");
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(getApplicationContext(),
+                                "Couldn't get json from server. Check LogCat for possible errors!",
+                                Toast.LENGTH_LONG).show();
+                    }
+                });
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            super.onPostExecute(result);
+            ListAdapter adapter = new SimpleAdapter(MainActivity.this, contactList,
+                    R.layout.list_item, new String[]{ "id","title"},
+                    new int[]{R.id.id, R.id.title});
+            lv.setAdapter(adapter);
         }
     }
-
-    //////////////////////////////////////
-    /*private void loadTaskList(){
-        ArrayList<String> taskList = dbHelper.getTaskList();
-
-
-        //Adapter add and clear
-    }*/
-    //////////////////////////////////////
-
 }
-
-
-
